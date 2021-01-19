a3a3865345fbd5172a54cd172b1f769d27fb8449
karunasree <31541182+karunasree@users.noreply.github.com>
2021-01-18 23:20:18 -0500

    IS

--- Program.cs
+++ Program.cs
@@ -3,8 +3,46 @@ using System.Collections;
 
 namespace EventsSample
 {
+	// Step 1 - Creating a class to pass arguments for the event handlers 	
+	public class HelloEventArgs : EventArgs
+	{
+		public String message { get; set; }
+		public HelloEventArgs(String Message)
+		{
+			this.message = Message;
+		}
+	}
 
-	using System;
+	// Step 2 - Setting up the delegate for the event
+	public delegate void HelloEventHandeler(object source, HelloEventArgs e);
+
+
+	// Step 3 - Declaring the code for the event
+	public class HelloEventClass
+	{
+		public event HelloEventHandeler HelloEvent;
+
+		public void Greeting()
+		{
+			HelloEventHandeler greet = HelloEvent;
+			if (HelloEvent != null)
+			{
+				greet(this, new HelloEventArgs("Say Hello"));
+			}
+
+		}
+	}
+
+	// Step 4 - Creating code that should occur when the event happens
+	public class Invoke
+	{		
+
+		public void HandleMessage(object sender, HelloEventArgs e)
+		{
+			Console.WriteLine("Message :{0}", e.message);
+		}
+
+	}
 
 	public class Program
 	{
@@ -12,58 +50,15 @@ namespace EventsSample
 		{
 			//Step 5 - Associate the event with the handler
 			var invoke = new Invoke();
-			invoke.Text="Say Hello"
 
-			var sayHello = new HelloClass();
-			sayHello.HelloEvent += HandleMessaage;
-				
+			var eventt= new HelloEventClass();
+			eventt.HelloEvent += invoke.HandleMessage;
 
 
-
-			
+			//Step 6 - Causing the event to occur
+			eventt.Greeting();
 		}
 	}
 
-	// Step 4 - Creating code that should occur when the event happens
-	public class Invoke
-	{
-		public string Text { get; set; }
 
-		public void HandleMessage(object sender, SayHelloEventHandeler e)
-		{
-			Console.WriteLine("Message {0}", e.ToString());
-		}
-
-	}
-
-	// Step 3 - Declaring the code for the event
-	public class HelloClass
-	{
-		public event SayHelloEventHandeler HelloEvent;
-
-		public void Hello()
-		{			
-			SayHelloEventHandeler greeting = HelloEvent;
-			if (HelloEvent != null)
-			{
-				greeting(this, new SayHelloEventArgs("Say Hello"));
-			}
-
-		}
-	}
-
-	// Step 2 - Setting up the delegate for the event
-	public delegate void SayHelloEventHandeler(object source, SayHelloEventArgs e);
-
-
-	// Step 1 - Creating a class to pass arguments for the event handlers 	
-	public class SayHelloEventArgs : EventArgs
-	{
-		public String message { get; set; }
-		public SayHelloEventArgs(string mess)
-		{
-			this.message = mess;
-
-		}
-	}
 }
